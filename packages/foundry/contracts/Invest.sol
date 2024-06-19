282043f4a5850ba6996630b04098d04b15428538
Youtpout <EddyBoughioul@hotmail.fr>
2024-06-19 19:39:28 +0200

    add contract to invest

---
+++ packages/foundry/contracts/Invest.sol
@@ -0,0 +1,114 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+import "forge-std/console.sol";
+
+// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+// import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * A smart contract that allows changing a state variable of the contract and tracking the changes
+ * It also allows the owner to withdraw the Ether in the contract
+ * @author BuidlGuidl
+ */
+contract Invest {
+    // State Variables
+    address public immutable owner;
+    IWETH public immutable weth;
+    ISwapRouter02 public immutable router;
+
+    // Constructor: Called once on contract deployment
+    // Check packages/foundry/deploy/Deploy.s.sol
+    constructor(address _owner, address _weth, address _router) {
+        owner = _owner;
+        weth = IWETH(_weth);
+        router = ISwapRouter02(_router);
+    }
+
+    function InvestNative(
+        address counterPart,
+        uint24 fee,
+        uint256 tickLower,
+        uint256 tickUpper
+    ) public payable returns (uint256) {
+        uint256 amount = 0;
+        weth.deposit{value: msg.value}();
+        return 0;
+    }
+
+    function _swapExactInputSingleHop(
+        uint256 amountIn,
+        uint256 amountOutMin
+    ) private {
+        weth.transfer(address(this), amountIn);
+        weth.approve(address(router), amountIn);
+
+        ISwapRouter02.ExactInputSingleParams memory params = ISwapRouter02
+            .ExactInputSingleParams({
+                tokenIn: address(weth),
+                tokenOut: address(weth),
+                fee: 500,
+                recipient: msg.sender,
+                amountIn: amountIn,
+                amountOutMinimum: amountOutMin,
+                sqrtPriceLimitX96: 0
+            });
+
+        router.exactInputSingle(params);
+    }
+}
+
+interface ISwapRouter02 {
+    struct ExactInputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 amountIn;
+        uint256 amountOutMinimum;
+        uint160 sqrtPriceLimitX96;
+    }
+
+    function exactInputSingle(
+        ExactInputSingleParams calldata params
+    ) external payable returns (uint256 amountOut);
+
+    struct ExactOutputSingleParams {
+        address tokenIn;
+        address tokenOut;
+        uint24 fee;
+        address recipient;
+        uint256 amountOut;
+        uint256 amountInMaximum;
+        uint160 sqrtPriceLimitX96;
+    }
+
+    function exactOutputSingle(
+        ExactOutputSingleParams calldata params
+    ) external payable returns (uint256 amountIn);
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+}
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256 amount) external;
+}
